a0452526646219e7ac73ebf457f32952a198d859
bftv <bftvtech@ucdavis.edu>
2021-02-04 07:42:16 -0800

    Create script.js

---
+++ vue_blocks/vue_cpsite_block/script.js
@@ -0,0 +1,80 @@
+/* Main URLs */
+mainURL = 'https://caes-cp-reports.caes.ucdavis.edu/cpweb/';
+const blockID = document.getElementsByClassName('vue-cpsite-block')[0].id;
+window.onmessage = function(event){
+    if (event.data[0] == 'pageHeight') {
+		var h = event.data[1] + 700;
+		document.getElementById('cpsite-frame').height = h+"px";
+    }
+};
+/* End Main URLs */
+
+
+/* Components */
+
+var cpsiteCmp = Vue.extend({
+    template: '#cpsite',
+
+    data: function() {
+        return {            
+			loading: true,
+			iframeurl: '',
+			iframesrc: '',
+			elframe: '',
+			height: '1000px',
+			test: '2'
+			
+        }
+    },
+	
+	mounted: function() {		
+		this.getURL();
+		this.loading = false;
+		
+		
+		document.onreadystatechange = () => { 
+			if (document.readyState == "complete") {			
+				//some code if needed
+				
+			}
+		}
+		
+	},
+
+    methods: {        
+		getURL: function(){						
+			this.iframesrc = mainURL
+		},
+	}
+})
+
+
+/* End Components */
+
+/* Router */
+
+var router = new VueRouter({
+	mode: 'history',
+	scrollBehavior() {
+		return { x: 0, y: 0 };
+	},	
+	
+	routes: [
+		{ 
+			path: '*', 
+			component: cpsiteCmp,
+			props: true
+		}
+	]
+});
+
+/* End Router */
+
+/* Initialize */
+
+new Vue({
+	el: '#cpsite-block',
+	router
+})
+
+/* End Initialize */
