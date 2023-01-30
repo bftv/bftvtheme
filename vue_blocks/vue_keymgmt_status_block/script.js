870a751804d3bc3f5e2d4b7db70114c1b8a84c07
bftv <bftvtech@ucdavis.edu>
2023-01-30 13:11:36 -0800

    Create script.js

---
+++ vue_blocks/vue_keymgmt_status_block/script.js
@@ -0,0 +1,72 @@
+/* Main URLs */
+mainURL = 'https://web.bftv.ucdavis.edu/keycardmgmt/status/';
+const blockID = document.getElementsByClassName('vue-keymgmt-status-block')[0].id;
+window.onmessage = function(event){
+    if (event.data[0] == 'pageHeight') {
+	   document.getElementById('keymgmt-status-frame').height = event.data[1]+"px";
+    }
+};
+/* End Main URLs */
+
+
+/* Components */
+
+var keymgmtCmp = Vue.extend({
+    template: '#keymgmt',
+
+    data: function() {
+        return {
+			iframeurl: '',
+      iframeln: '',
+			iframesrc: '',
+			height: '1000px'
+        }
+    },
+
+	mounted: function() {
+		this.getURL();
+	},
+
+    methods: {
+		getURL: function(){
+			this.iframeurl = this.$route.query.ksid;
+      this.iframeln = this.$route.query.lastname;
+			if(this.iframeurl && this.iframeln){
+				this.iframesrc = mainURL+'status.php?ksid='+this.iframeurl+'&&lastname='+this.iframeln;
+			} else {
+				this.iframesrc = mainURL+'status.php';
+			}
+		},
+	}
+})
+
+
+/* End Components */
+
+/* Router */
+
+var router = new VueRouter({
+	mode: 'history',
+	scrollBehavior() {
+		return { x: 0, y: 0 };
+	},
+
+	routes: [
+		{
+			path: '*',
+			component: keymgmtCmp,
+			props: true
+		}
+	]
+});
+
+/* End Router */
+
+/* Initialize */
+
+new Vue({
+	el: '#keymgmt-status-block',
+	router
+})
+
+/* End Initialize */
